2a6a770dde5c2fe45a02a81b1e29b404d917a495
andrewhamili <andrewhamilidevelopment@gmail.com>
2024-08-16 13:38:18 +0800

    canvas for horizontal rule

--- src/pdfDef.ts
+++ src/pdfDef.ts
@@ -6,21 +6,25 @@ const imageList: Record<string, ImageDefinition> = {
   },
 };
 export const pdfDef: TDocumentDefinitions = {
-  content: [{text: "Hello"},{
-    canvas: [
-      {
-        type: 'line',
-        x1: 0,
-        y1: 0,
-        x2: 514,
-        y2: 0,
-        lineWidth: 1,
-        lineColor: '#000000',
-      },
-    ],
-  },{text: "Hello"},],
+  content: [
+    { text: 'Hello' },
+    {
+      canvas: [
+        {
+          type: 'line',
+          x1: 0,
+          y1: 0,
+          x2: 514,
+          y2: 0,
+          lineWidth: 1,
+          lineColor: '#000000',
+        },
+      ],
+    },
+    { text: 'Hello' },
+  ],
   images: imageList,
   defaultStyle: {
     font: 'Helvetica',
-  }
+  },
 };
